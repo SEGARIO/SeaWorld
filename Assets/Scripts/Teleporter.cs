adbe4a1bc97176716066217c7973be9e68874468
SEGARIO <nonokurod@gmail.com>
2026-08-10 03:35:35 +0200

    teleportation done

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -2,5 +2,40 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-   void ontr
+
+    public Transform _position;
+    public GameObject player;
+   public GameObject _cam;
+
+    public GameObject _circleIn;
+    public GameObject _circleOut;
+    public PlayerController playerController;
+    public CharacterController _ch;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger touché par : " + other.name);
+            Debug.Log("Référence player : " + player.name);
+            playerController.enabled = false;
+            _ch.enabled = false;
+            _circleOut.SetActive(false);
+            _circleIn.SetActive(true);
+            //player = other.gameObject;
+            Invoke("Teleport", 1);
+        }
+    }
+
+    void Teleport()
+    {
+        
+      
+        _circleOut.SetActive(true);
+        _circleIn.SetActive(false);
+        player.transform.position = _position.position;
+         _cam.gameObject.transform.position = _position.position;
+        playerController.enabled = true;
+        _ch.enabled = true;
+
+    }
 }
