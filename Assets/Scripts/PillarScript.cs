d385f335f62e0cdd5472b28aa6533cbe5e37c498
SEGARIO <nonokurod@gmail.com>
2026-08-19 01:30:25 +0200

    doing first room

--- Assets/Scripts/PillarScript.cs
+++ Assets/Scripts/PillarScript.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class PillarScript : MonoBehaviour
 {
+    public GameObject _pillar;
+    public ParticleSystem _s;
+    public GameObject _parent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void onc
+    private void OnTriggerEnter(Collider collision)
+    {
+        Destroy(_pillar);
+        Invoke("Destroyer", 2);
+        _s.Play();
+        GameFeel.Instance.PlayJuice(3f, 0.6f);
+    }
+
+    void Destroyer()
+    {
+        Destroy(_parent);
+    }
+
 }
