dd39fe5f2258f1bf3900e8b62b8a00088a860349
SEGARIO <nonokurod@gmail.com>
2026-08-22 00:55:04 +0200

    spaceshipgo up

--- Assets/Scripts/AdvancingScript.cs
+++ Assets/Scripts/AdvancingScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class AdvancingScript : MonoBehaviour
 {
     public float _speed;
+    public Animator _anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,5 +14,11 @@ public class AdvancingScript : MonoBehaviour
     void Update()
     {
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + Time.deltaTime * _speed);
+
+        if(this.transform.position.z >= 2367)
+        {
+            _anim.enabled = true;
+            _anim.SetTrigger("Up");
+        }
     }
 }
