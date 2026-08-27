f2929dc6a8b4455a67825ca7ac12d0f4cb43b5ef
SEGARIO <nonokurod@gmail.com>
2026-08-27 21:16:32 +0200

    press a bug fixed

--- Assets/Scripts/LookAtFixedPoint.cs
+++ Assets/Scripts/LookAtFixedPoint.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class LookAtFixedPoint : MonoBehaviour
 {
+    public GameObject _pressA;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +13,6 @@ public class LookAtFixedPoint : MonoBehaviour
     void Update()
     {
         transform.LookAt(new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 1));
+        _pressA.transform.position = new Vector3(this.transform.position.x - 0.3f, _pressA.transform.position.y, _pressA.transform.position.z);
     }
 }
