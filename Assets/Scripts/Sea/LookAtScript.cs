c3dfe26cc7897d7d626fc8b6da1cc9a110135c36
SEGARIO <nonokurod@gmail.com>
2026-09-02 02:14:11 +0200

    doing vising gun

--- Assets/Scripts/Sea/LookAtScript.cs
+++ Assets/Scripts/Sea/LookAtScript.cs
@@ -5,6 +5,12 @@ public class LookAtScript : MonoBehaviour
 {
     public GameObject _target;
     public bool _followsGun;
+    public TriggerShoot _shoot;
+    bool _canShoot;
+
+    [Header("Arms")]
+    public Transform _arm1;
+    public Transform _arm2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,13 +26,21 @@ public class LookAtScript : MonoBehaviour
         }
         else
         {
-            if (Vector3.Distance(transform.position, _target.transform.position) > 2f)
+            if (Vector3.Distance(transform.position, _target.transform.position) > 2)
             {
                 transform.LookAt(_target.transform);
+                _canShoot = true;
+                _arm2.transform.localRotation = Quaternion.Euler(-29.828f, -20.904f, 18.874f);
+                _shoot.enabled = true;
+                Debug.Log("Proche");
             }
             else
             {
-
+                _shoot.enabled = false;
+                _arm2.transform.localRotation = Quaternion.Euler(-10.801f, 87.681f, 10.33f);
+                _arm1.transform.localRotation = Quaternion.Euler(37.822f, 37.822f, 576.116f);
+                Debug.Log("Loins");
+                _canShoot = false;
             }
         }
        
