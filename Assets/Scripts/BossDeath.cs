03dbb96ac5f00ecc3f3c28038492b92cd9c6d61c
SEGARIO <nonokurod@gmail.com>
2026-09-25 03:47:46 +0200

    doing integratio boss

--- Assets/Scripts/BossDeath.cs
+++ Assets/Scripts/BossDeath.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossDeath : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
-        
+        Invoke("ChangeScene", 10);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("SpaceTravel");
     }
 }
